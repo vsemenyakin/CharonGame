96c2fc2bfd895c639c9e73867052d0679bb97902
Vano <iw.lasgin2011@gmail.com>
2018-01-28 14:00:54 +0300

    Commit of last update

---
+++ Assets/SimpleParalax.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+
+public class SimpleParalax : MonoBehaviour { 
+
+public float paralaxFactor = 0.0f; 
+
+// Use this for initialization 
+void Start () { 
+} 
+
+// Update is called once per frame 
+void FixedUpdate () { 
+float theCameraPosition = Camera.main.transform.position.x; 
+
+Vector3 thePosition = transform.position; 
+thePosition.x = theCameraPosition * paralaxFactor; 
+transform.position = thePosition; 
+} 
+}
