004a3ad447926fb3cbd46e6f99286d989ded26b1
vsemenyakin <semenyakinVS@gmail.com>
2018-01-28 14:59:59 +0200

    Added background manager for gradient background

---
+++ Assets/BackgroundController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+
+    public float[] places;
+    public Sprite[] sprites;
+
+    public SpriteRenderer render;
+
+	// Use this for initialization
+	void Start () {
+//        render.sprite = sprites[];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
